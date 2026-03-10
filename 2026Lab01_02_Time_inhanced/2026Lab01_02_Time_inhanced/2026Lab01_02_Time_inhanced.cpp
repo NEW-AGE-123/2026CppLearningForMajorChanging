8d6b34a16104b36eed4976bcde0473fb71071660
Twistzz <2459686733@qq.com>
2026-03-10 22:51:35 +0800

    新增03，02

---
+++ 2026Lab01_02_Time_inhanced/2026Lab01_02_Time_inhanced/2026Lab01_02_Time_inhanced.cpp
@@ -0,0 +1,61 @@
+﻿// 2026Lab01_02_Time_inhanced.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+class Time
+{
+	friend void tick(Time& );
+	friend ostream& operator<<(ostream& , Time );
+public:
+	Time(int a, int b, int c)
+	{
+		hour = a;
+		minute = b;
+		second = c;
+	}
+
+
+private:
+	int hour;
+	int minute;
+	int second;
+};
+
+
+void tick(Time& t)
+{
+	int*p[3] = { &t.second ,&t.minute,&t.hour };
+	int**a = &p[0];
+	**a = **a + 1;
+	while ((**a)==60&&(a)!=(p+2))
+	{
+		**(a + 1) = **(a + 1) + 1;
+		**a = 0;
+		a = a + 1;
+	}
+	if ((**a) == 24 && (a) == (p + 2))
+	{
+		t.hour = 0;
+		t.minute = 0;
+		t.second = 0;
+	}
+}
+
+ostream& operator<<(ostream& output, Time t)
+{
+	output << setw(2) << setfill('0') << t.hour << ":" << setw(2) << setfill('0') << t.hour << ":" << setw(2) << setfill('0') << t.second << endl;
+	return output;
+}
+int main()
+{
+	Time a(23, 59, 59);
+	cout << a;
+	tick(a);
+	cout << a;
+
+}
+
---
+++ 2026Lab01_03_HugeInteger/2026Lab01_03_HugeInteger/2026Lab01_03_HugeInteger.cpp
@@ -0,0 +1,194 @@
+﻿// 2026Lab01_03_HugeInteger.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <cmath>
+#include<sstream>
+using namespace std;
+
+class HugeInteger
+{
+	friend ostream& operator<<(ostream& output, HugeInteger h);
+	friend HugeInteger operator+(HugeInteger a, HugeInteger b);
+	friend HugeInteger operator-(HugeInteger a, HugeInteger b);
+public:
+	HugeInteger()
+	{
+		for (int &a : number)
+		{
+			a = 0;
+		}
+	}
+	HugeInteger(int a)
+	{
+		for (int &b : number)
+		{
+			b = 0;
+		}
+		input(a);
+	}
+	void input(int a)
+	{
+		int i = 0;
+		for (int &b : number)
+		{
+			b = 0;
+		}
+		while (a != 0)
+		{
+			number[i] = a % 10;
+			a = a / 10;
+			i++;
+		}
+	}
+	void output(int a)
+	{
+		cout << a << endl;
+	}
+	HugeInteger add(HugeInteger a)
+	{
+		HugeInteger b = *this;
+		return (a + b);
+	}
+	HugeInteger substract(HugeInteger a)
+	{
+		HugeInteger b = *this;
+		return (a - b);
+	}
+	bool isEqualto(HugeInteger b)
+	{
+		HugeInteger a = *this;
+		for (int i = 0; i < 40; i++)
+		{
+			if (a.number[i] != b.number[i])
+				return 0;
+		}
+		return 1;
+	}
+	bool isNotEqualto(HugeInteger b)
+	{
+		HugeInteger a = *this;
+		for (int i = 0; i < 40; i++)
+		{
+			if (a.number[i] != b.number[i])
+				return 1;
+		}
+		return 0;
+	}
+	bool isGreaterThan(HugeInteger b)
+	{
+		HugeInteger a = *this;
+		for (int i = 39; i > -1; i--)
+		{
+			if (a.number[i] > b.number[i])
+				return 1;
+			if (a.number[i] < b.number[i])
+			{
+				return 0;
+			}
+		}
+		return 0;
+	}
+	bool isLessThan(HugeInteger b)
+	{
+		HugeInteger a = *this;
+		for (int i = 39; i > -1; i++)
+		{
+			if (a.number[i] < b.number[i])
+				return 1;
+			if (a.number[i] > b.number[i])
+				return 0;
+		}
+		return 0;
+	}
+	bool isGreaterThanOrEqualto(HugeInteger b)
+	{
+		HugeInteger a = *this;
+		for (int i = 39; i > -1; i--)
+		{
+			if (a.number[i] > b.number[i])
+				return 1;
+			if (a.number[i] < b.number[i])
+				return 0;
+		}
+		return 1;
+	}
+	bool isLessThanOrEqualto(HugeInteger b)
+	{
+		HugeInteger a = *this;
+		for (int i = 39; i > -1; i++)
+		{
+			if (a.number[i] < b.number[i])
+				return 1;
+			if (a.number[i] > b.number[i])
+				return 0;
+		}
+		return 1;
+	}
+	bool isZero()
+	{
+		for (size_t i = 0; i < 40; i++)
+		{
+			if ((*this).number[i] != 0)
+				return 0;
+		}
+		return 1;
+	}
+private:
+	int number[40];
+};
+
+ostream& operator<<(ostream& output, HugeInteger h)
+{
+	stringstream  iss;
+	for (int i = 39; i > -1; i--)
+	{
+		iss << h.number[i];
+	}
+	string integer = iss.str();
+	output << integer << endl;
+	return output;
+}
+
+HugeInteger operator+(HugeInteger a, HugeInteger b)
+{
+	HugeInteger temp;
+	for (int i = 0; i < 40; i++)
+	{
+		temp.number[i] = temp.number[i]+a.number[i] + b.number[i];
+		if (temp.number[i]>=10)
+		{
+			temp.number[i] = temp.number[i] - 10;
+			temp.number[i + 1]++;
+		}
+	}
+	return temp;
+}
+HugeInteger operator-(HugeInteger a, HugeInteger b)
+{
+	if (!a.isGreaterThanOrEqualto(b))
+		swap(a, b);
+	HugeInteger temp;
+	for (int i = 0; i < 40; i++)
+	{
+		temp.number[i] = temp.number[i] + a.number[i] - b.number[i];
+		if (temp.number[i] <0)
+		{
+			temp.number[i] = temp.number[i] + 10;
+			temp.number[i + 1]--;
+		}
+	}
+	return temp;
+}
+
+
+
+
+int main()
+{
+	HugeInteger a(923231);
+	HugeInteger b(92328);
+	cout << a+b;
+	cout << a - b;
+}
+
